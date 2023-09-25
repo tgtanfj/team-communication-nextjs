768eebd83483dc952e09e11108cc328a188bbd3d
trungtan <trungtan14@gmail.com>
2023-09-25 23:16:11 +0700

    fix server-search component

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -2,6 +2,8 @@
 
 import { Search } from "lucide-react";
 import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+
 import {
   CommandDialog,
   CommandEmpty,
@@ -9,8 +11,7 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-} from "../ui/command";
-import { useParams, useRouter } from "next/navigation";
+} from "@/components/ui/command";
 
 interface ServerSearchProps {
   data: {
@@ -18,9 +19,9 @@ interface ServerSearchProps {
     type: "channel" | "member";
     data:
       | {
-          id: string;
-          name: string;
           icon: React.ReactNode;
+          name: string;
+          id: string;
         }[]
       | undefined;
   }[];
@@ -28,7 +29,6 @@ interface ServerSearchProps {
 
 export const ServerSearch = ({ data }: ServerSearchProps) => {
   const [open, setOpen] = useState(false);
-
   const router = useRouter();
   const params = useParams();
 
@@ -54,7 +54,7 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
     setOpen(false);
 
     if (type === "member") {
-      return router.push(`/servers/${params.serverId}/conversations/${id}`);
+      return router.push(`/servers/${params?.serverId}/conversations/${id}`);
     }
 
     if (type === "channel") {
@@ -69,11 +69,11 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
         className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
       >
         <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />
-        <p className="transition font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300">
-          search
+        <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
+          Search
         </p>
-        <kbd className="poiter-events-none inline-flex h-5 select-none items-center gap-1 rounded-border bg-muted px-1.5 font-mono text-[10px] font font-medium text-muted-foreground ml-auto">
-          <span className="text-xs">CTRL</span>K
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
+          <span className="text-xs">⌘</span>K
         </kbd>
       </button>
       <CommandDialog open={open} onOpenChange={setOpen}>
